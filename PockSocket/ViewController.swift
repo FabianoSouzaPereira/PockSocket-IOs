d57b4bcb91180730b3d93ebd90c7e29d6165e90d
Fabiano <fabiano.pereira@seventh.com.br>
2022-06-22 11:00:25 -0300

    Initial Commit

---
+++ PockSocket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PockSocket
+//
+//  Created by Fabiano Pereira on 22/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PockSocketUITests/PockSocketUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PockSocketUITestsLaunchTests.swift
+//  PockSocketUITests
+//
+//  Created by Fabiano Pereira on 22/06/22.
+//
+
+import XCTest
+
+class PockSocketUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
